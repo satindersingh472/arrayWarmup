2cebeb7c2f32c4e41d8b280a8c59e00cd2a65cf2
satinder <satindersingh472@gmail.com>
2022-07-09 19:21:12 -0600

    update

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 let student_name = "satinder";
 let student_gpa = 3.5;
-let honor_role = true;
+let honor_role = false;
 if (honor_role === true) {
     console.log(`congrats ${student_name} you are on the honor's list.`);
 } else {
@@ -20,15 +20,20 @@ if (student_gpa >= 0 && student_gpa <= 1.9) {
 
 let student_names = [`sam`, `mani`, `nani`];
 let student_gpas = [2, 3, 4];
-let honor_roles = [false, true, true];
+let honor_roles = [false,true, false];
+if((honor_roles[0] === true) && (honor_roles[1] === true) && (honor_roles[2] === true)){
+    console.log(`all student are honored`);
+} else if ((honor_roles[0] === true ) && (honor_roles[1] === true || honor_roles[1] === false) && (honor_roles[2] === true || honor_roles[2]===false)){
+    console.log(`some of them did not make it.`);
+} else if (honor_roles[0]===false && honor_roles[1] === false && honor_roles[2] === false ){
+    console.log(`All of them failed`);
+}
 
 student_names.push(`nit`);
 student_gpas.push(3);
 honor_roles.push(true);
-console.log(student_names);
 
 let last_student = student_names.pop();
-console.log(student_names);
 let last_student_gpa = student_gpas.pop();
 let last_student_honor = honor_roles.pop();
 if (last_student_gpa > student_gpas){
