6bbdec339222d057201922046b7a1ed3dc4cbe33
satinder <satindersingh472@gmail.com>
2022-07-12 16:00:29 -0600

    finished

--- js/app.js
+++ js/app.js
@@ -20,22 +20,23 @@ if (student_gpa >= 0 && student_gpa <= 1.9) {
 
 let student_names = [`sam`, `mani`, `nani`];
 let student_gpas = [2, 3, 4];
-let honor_roles = [false,true, false];
+let honor_roles = [false,true,true];
 if((honor_roles[0] === true) && (honor_roles[1] === true) && (honor_roles[2] === true)){
     console.log(`all student are honored`);
-} else if ((honor_roles[0] === true ) && (honor_roles[1] === true || honor_roles[1] === false) && (honor_roles[2] === true || honor_roles[2]===false)){
+} else if ((honor_roles[0] === true && honor_roles[1] === true && honor_roles[2] === false ) || 
+( honor_roles[0] === false && honor_roles[1] === true && honor_roles[2] === true  )|| (honor_roles[0] === true && honor_roles[1] === true && honor_roles[2]===false)){
     console.log(`some of them did not make it.`);
 } else if (honor_roles[0]===false && honor_roles[1] === false && honor_roles[2] === false ){
     console.log(`All of them failed`);
 }
 
 student_names.push(`nit`);
-student_gpas.push(3);
+student_gpas.push(5);
 honor_roles.push(true);
 
 let last_student = student_names.pop();
 let last_student_gpa = student_gpas.pop();
 let last_student_honor = honor_roles.pop();
-if (last_student_gpa > student_gpas){
+if ((last_student_gpa > student_gpas[0]) && (last_student_gpa > student_gpas[1]) && (last_student_gpa > student_gpas[2]) ){
 console.log(`last student is the best`);
 }
